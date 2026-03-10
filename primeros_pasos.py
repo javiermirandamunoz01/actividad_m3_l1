59242cc23959e372dd353495b4d1a1c50bc17317
javiermirandamunoz01 <javiermirandamunoz01@gmail.com>
2026-03-10 16:02:28 +0000

    trabajo

---
+++ primeros_pasos.py
@@ -0,0 +1,37 @@
+# 1. Mostrar un saludo
+# print sirve para escribir texto en la pantalla
+print("¡Hola! Este es mi primer programa en Python.")
+
+
+# 2. Guardar datos en variables
+nombre = "javier miranda"    # Guardamos un texto
+edad = 32              # Guardamos un número
+ciudad = "Codegua"      # Guardamos otro texto
+
+# Mostramos lo que guardamos arriba
+print(nombre)
+print(edad)
+print(ciudad)
+
+
+
+# 3. Sumar dos números
+numero1 = 15            # Primer número
+numero2 = 10            # Segundo número
+suma = numero1 + numero2 # Sumamos los dos y guardamos el resultado
+
+
+print("El total de la suma es:")
+print(suma)
+
+
+
+# 4. Usar la calculadora de Python
+import math             # Traemos herramientas matemáticas
+
+numero = 9             # El número al que le sacaremos la raíz
+raiz = math.sqrt(numero) # Calculamos la raíz cuadrada
+
+# Mostramos el resultado de la raíz
+print("La raíz cuadrada es:")
+print(raiz)
